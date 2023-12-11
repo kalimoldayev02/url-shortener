35fc8c02e3ba36b4b000fd9cf1782d97c107dedf
kalimoldayev02 <kalimoldayev02@gmail.com>
2023-12-11 22:32:18 +0600

    fix(Postgres): исправил создания таблиц

--- internal/repository/storage/postgres/postgres.go
+++ internal/repository/storage/postgres/postgres.go
@@ -18,20 +18,20 @@ func New(storagePath string) (*Storage, error) {
 		return nil, fmt.Errorf("%s: %w", operation, err)
 	}
 
-	stmt, err := db.Prepare(
-		`CREATE TABLE IF NOT EXIST url(
-		id SERIAL PRIMARY KEY,
-		alias TEXT NOT NULL UNIQUE,
-		url TEXT NOT NULL
-	); 
-	CREATE INDEX IF NOT EXIST idx_alias ON url(alias)`)
-
-	if err != nil {
-		return nil, fmt.Errorf("%s: %w", operation, err)
+	queries := []string{
+		`CREATE TABLE IF NOT EXISTS url (
+			id SERIAL PRIMARY KEY,
+			alias TEXT NOT NULL UNIQUE,
+			url TEXT NOT NULL
+		);`,
+		`CREATE INDEX IF NOT EXISTS idx_alias ON url(alias);`,
 	}
+	for _, query := range queries {
+		if _, err := db.Exec(query); err != nil {
+			// обработка ошибки, если что-то пошло не так
+			return nil, fmt.Errorf("%s: %w", operation, err)
 
-	if _, err := stmt.Exec(); err != nil {
-		return nil, fmt.Errorf("%s: %w", operation, err)
+		}
 	}
 
 	return &Storage{db: db}, nil
