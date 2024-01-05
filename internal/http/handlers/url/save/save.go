ce31835dea66df5a3cf6e8be28339beacfe85cc8
kalimoldayev02 <kalimoldayev02@gmail.com>
2024-01-05 09:28:32 +0600

    feat(Handlers): save handler

--- internal/http/handlers/url/save/save.go
+++ internal/http/handlers/url/save/save.go
@@ -4,7 +4,11 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
 	resp "github.com/kalimoldayev02/url/pkg/api/response"
+	"github.com/kalimoldayev02/url/pkg/lib/logger/sl"
+	"github.com/kalimoldayev02/url/pkg/random"
 	"golang.org/x/exp/slog"
 )
 
@@ -18,11 +22,14 @@ type Response struct {
 	Alias string `json:"alias, omitempty"`
 }
 
-type UrlServer interface {
+type URLSaver interface {
 	Save(urlToSave, alias string) (int64, error)
 }
 
-func New(log *slog.Logger, urlServer UrlServer) http.HandlerFunc {
+// TODO: move to config
+const aliasLength = 4
+
+func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		const operation = "handlers.url.save"
 		var req Request
@@ -32,5 +39,33 @@ func New(log *slog.Logger, urlServer UrlServer) http.HandlerFunc {
 			slog.String("request_id", middleware.GetReqID(r.Context())),
 		)
 
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode request body", sl.Err(err))
+			render.JSON(w, r, resp.Error("failed to decode request"))
+
+			return
+		}
+
+		log.Info("request body decoded", slog.Any("reuest", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			validateErr := err.(validator.ValidationErrors)
+			log.Error("invalid request", sl.Err(err))
+
+			render.JSON(w, r, resp.ValidationError(validateErr))
+
+			return
+		}
+
+		alias := req.Alias
+		if alias == "" {
+			alias = random.NewRandomString(aliasLength)
+		}
+
+		id, err := urlSaver.Save(req.Url, req.Alias)
+		if err != nil {
+
+		}
 	}
 }
--- pkg/api/response/response.go
+++ pkg/api/response/response.go
@@ -1,5 +1,12 @@
 package response
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
 type Response struct {
 	Status string `json:"status"`
 	Error  string `json:"error, omitempty"`
@@ -20,5 +27,25 @@ func Error(message string) Response {
 	return Response{
 		Status: StatusError,
 		Error:  message,
+	}
+}
+
+func ValidationError(errs validator.ValidationErrors) Response {
+	var errMessages []string
+
+	for _, err := range errs {
+		switch err.ActualTag() {
+		case "required":
+			errMessages = append(errMessages, fmt.Sprintf("field %s is a required field", err.Field()))
+		case "url":
+			errMessages = append(errMessages, fmt.Sprintf("field %s is not a valid URL", err.Field()))
+		default:
+			errMessages = append(errMessages, fmt.Sprintf("field %s is not a valid", err.Field()))
+		}
+	}
+
+	return Response{
+		Status: StatusError,
+		Error:  strings.Join(errMessages, ", "),
 	}
 }
